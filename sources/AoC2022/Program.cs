f3999daa2326789119d125b28214e1788b5f1f13
Tamás Deme <tomzorz@outlook.com>
2022-12-03 14:53:44 +0100

    repo init and day 1

---
+++ sources/AoC2022/Program.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+
+var data = (await File.ReadAllLinesAsync("input.txt")).ToList();
+
+/*
+
+PartOne
+
+*/
+
+data = @"".Split('\n').Select(x => x.Trim()).ToList();
+
+
+
+Debugger.Break();
+
+/*
+
+PartTwo
+
+*/
+
+
+Debugger.Break();
+
+/* methods */
